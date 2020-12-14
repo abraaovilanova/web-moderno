bd1f5c15b21057d6ed9c3382529ac446b14128a2
Abraão Vila Nova <73487632+abraaovilanova@users.noreply.github.com>
2020-12-14 09:41:16 -0300

    Update exercicio2.js

--- funcao/exercicios/exercicio2.js
+++ funcao/exercicios/exercicio2.js
@@ -9,3 +9,20 @@ Crie uma função que recebe os comprimentos dos três lados de um triângulo e 
 ao tamanho de seus lados. (Neste exemplo deve-se abstrair as condições matemáticas de existência de um
 triângulo).
 */
+
+function classificacaoTriangulo(a, b, c){
+
+    if (b + c > a && a + c > b && a + b > c){
+        if(a === b && a === c){
+            console.log('O triangulo é EQUILÁTERO')
+        }else if(a !== b && b === c || a === b && b !== c || a === c && b !== c){
+            console.log('O triangulo é ISÓSCELES')
+        }else{
+            console.log('O triangulo é ESCALENO')
+        }
+    }else{
+        console.log('Não é triangulo')
+    }
+}
+
+classificacaoTriangulo(4,4,5)
