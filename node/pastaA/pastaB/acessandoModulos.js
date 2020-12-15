f0b811025e84978da2d48c8e3160bee944e8d559
Abraão Vila Nova <73487632+abraaovilanova@users.noreply.github.com>
2020-12-15 13:04:41 -0300

    aulas node

---
+++ node/pastaA/pastaB/acessandoModulos.js
@@ -0,0 +1,17 @@
+const moduloA = require('../../moduloA')
+const moduloB = require('../../moduloB')
+console.log(moduloA.ola)
+console.log(moduloB)
+
+
+const C = require('./pastaC')
+console.log(C.ola)
+/*
+const http = require('http')
+http.createServer((req, res) => {
+    res.write('Bom dia')
+    res.end()
+}).listen(8080)
+*/
+
+
